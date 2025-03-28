1d803e145391d1c2aa3a3acbfdde8169aeed013e
Merlijn Wajer <merlijn@wizzup.org>
2025-03-28 17:47:23 +0100

    boards: raspi3 experimental changes

--- boards/raspberry-pi3.sh
+++ boards/raspberry-pi3.sh
@@ -19,13 +19,11 @@
 
 ## settings & config
 vars+=(device_name arch size parted_type parted_boot parted_root bootfs inittab)
-vars+=(gitkernel gitbranch rpifirmware)
-arrs+=(custmodules)
 
 device_name="raspi3"
 arch="arm64"
-size=3000
-inittab=("T0:23:respawn:/sbin/agetty -L ttyAMA0 115200 vt100")
+size=4000
+inittab=("T0:23:respawn:/sbin/agetty -L ttyS0 115200 vt100")
 
 parted_type="dos"
 bootfs="vfat"
@@ -33,82 +31,19 @@ rootfs="ext4"
 dos_boot="fat32 2048s 264191s"
 dos_root="$rootfs 264192s 100%"
 
-extra_packages+=()
-custmodules=(snd_bcm2835)
-
-gitkernel="https://github.com/raspberrypi/linux"
-gitbranch="rpi-4.16.y"
 rpifirmware="https://github.com/raspberrypi/firmware.git"
 
-
-prebuild() {
-	fn prebuild
-	req=(device_name strapdir)
-	ckreq || return 1
-
-	notice "executing $device_name prebuild"
-
-	mkdir -p $R/tmp/kernels/$device_name
-}
-
-postbuild() {
-	fn postbuild
-
-	notice "executing $device_name postbuild"
-
-	copy-root-overlay
-	postbuild-clean
-}
+extra_packages+=()
 
 build_kernel_arm64() {
-	fn build_kernel_armhf
-	req=(R arch device_name gitkernel gitbranch rpifirmware)
-	req+=(strapdir)
-	ckreq || return 1
+    /bin/true
 
-	notice "building $arch kernel"
-
-	prebuild || zerr
-
-	get-kernel-sources || zerr
-	pushd $R/tmp/kernels/$device_name/${device_name}-linux
-		# pi3 defconfig
-		make \
-			$MAKEOPTS \
-			ARCH=arm64 \
-			CROSS_COMPILE=$compiler \
-				bcmrpi3_defconfig || zerr
-
-		# compile kernel and modules
-		make \
-			$MAKEOPTS \
-			ARCH=arm64 \
-			CROSS_COMPILE=$compiler || zerr
-
-		# install kernel modules
-		sudo -E PATH="$PATH" \
-			make \
-				$MAKEOPTS \
-				ARCH=arm64 \
-				CROSS_COMPILE=$compiler \
-				INSTALL_MOD_PATH=$strapdir \
-					modules_install || zerr
-	popd
-
-	clone-git "$rpifirmware" "$R/tmp/kernels/$device_name/${device_name}-firmware"
+    clone-git "$rpifirmware" "$R/tmp/kernels/$device_name/${device_name}-firmware"
 	sudo cp $R/tmp/kernels/$device_name/${device_name}-firmware/boot/bootcode.bin "$strapdir/boot/"
 	sudo cp $R/tmp/kernels/$device_name/${device_name}-firmware/boot/fixup* "$strapdir/boot/"
 	sudo cp $R/tmp/kernels/$device_name/${device_name}-firmware/boot/start* "$strapdir/boot/"
 	sudo cp $R/tmp/kernels/$device_name/${device_name}-firmware/boot/COPYING.linux "$strapdir/boot/"
 	sudo cp $R/tmp/kernels/$device_name/${device_name}-firmware/boot/LICENCE.broadcom "$strapdir/boot/"
 
-	pushd $R/tmp/kernels/$device_name/${device_name}-linux
-	#	sudo perl scripts/mkknlimg --dtok arch/arm64/boot/Image.gz $strapdir/boot/kernel8.img
-		sudo cp arch/arm64/boot/Image                 $strapdir/boot/kernel8.img
-		sudo cp arch/arm64/boot/dts/broadcom/bcm*.dtb $strapdir/boot/
-		sudo cp arch/arm64/boot/dts/overlays/*.dtbo   $strapdir/boot/overlays/
-		sudo cp arch/arm64/boot/dts/overlays/README   $strapdir/boot/overlays/
-	popd
-
-	postbuild || zerr
+	copy-root-overlay
 }
